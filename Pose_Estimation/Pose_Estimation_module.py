4f016c0fd6a1eb2231db3c91637ac4367d4e5516
Fatjri Zikri <66940604+fatjrizikri@users.noreply.github.com>
2023-10-08 09:22:43 +0700

    Update Pose_Estimation_module.py

--- Pose_Estimation/Pose_Estimation_module.py
+++ Pose_Estimation/Pose_Estimation_module.py
@@ -4,6 +4,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 class poseDetector():
     #this code basic code of object oriented programming
@@ -37,17 +38,47 @@ class poseDetector():
         return img
     
     def getPosition(self, img, draw = True):
-        lmList = []
+        self.lmList = []
         if self.results.pose_landmarks:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 # print(id, lm)
                 cx, cy = int(lm.x * w), int(lm.y * h)    
                 # cx, xy (possition id landmark), 10 (diameter of circle), 255,0,0 (Value color RGB Red, Grean, Blue)                
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx,cy), 10, (255, 0 , 0), cv2.FILLED)
-        return lmList
+        return self.lmList
+    
+    def findAngle(self, img, p1, p2, p3, draw = True):
+
+        # Get the landmark from id point landmarks pose estimation
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+        
+        #Caculate the angle use simple math trigonometri
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2)- math.atan2(y1 - y2, x1 - x2))
+        if angle < 0:
+            angle += 360
+            # angle = angle * -1
+        # print(angle)
+
+        # Draw
+        if draw:
+            # to Draw circle point pose we use and connect point pose landmarks we set
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1,y1), 10, (255, 0 , 0), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2,y2), 10, (255, 0 , 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3,y3), 10, (255, 0 , 0), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            # to write value of degree from three point pose landmarks we set
+            cv2.putText(img, str(int(angle)), (x2 - 20, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255))
+        
+        return angle
 
 def main():
     cap = cv2.VideoCapture('Video/2.mp4')
@@ -56,7 +87,7 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.findPose(img)
-        lmList = detector.getPosition(img)
+        lmList = detector.getPosition(img, draw = False)
         # print(lmList) # to print all data point position of pose all point is 33 point of landmarks pose full body
         
         if len(lmList) !=0: # check the points of body landmarks whether they exist or not to prevent errors
